5e267d7105c74dbb62a5bf8c159af706bfc2a83f
devizier <devizier@gmail.com>
2016-02-11 20:44:47 +0300

    Commit from Pycharm

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-# ---- Flask Hello World ---- #
-# import the Flask class from the flask package
+# import the Flask class from the flask module
 from flask import Flask
 
 # create the application object
 app = Flask(__name__)
 
-# use decorators to link the function to a url
-@app.route("/")
-@app.route("/hello")
+# error handling
+app.config["DEBUG"] = True
 
-# define the view using a function which returns a string
+#use decorators to link the function to url
+@app.route('/')
+@app.route('/hello')
 def hello_world():
-    return "Hello world!"
+    return "Hello, World!"
 
 # dynamic route
-@app.route("/test/<search_query>")
+@app.route('/test/<search_query>')
 def search(search_query):
     return search_query
 
-# start the development server using run() method
+# dynamic route with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
+#start the development server using the run() method
 if __name__ == "__main__":
     app.run()
